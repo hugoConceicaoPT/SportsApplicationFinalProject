f8e9f0949fc8041e385885ce38c63988cdabaac3
hugoConceicaoPT <hugoconceicao.TI@outlook.com>
2024-11-17 19:26:52 +0000

    Added login

--- server/build/routesUser.js
+++ server/build/routesUser.js
@@ -17,14 +17,39 @@ const bcrypt_1 = __importDefault(require("bcrypt"));
 const user_1 = __importDefault(require("./models/user"));
 const router = express_1.default.Router();
 router.post('/register', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email, password } = req.body;
-    const hash = yield bcrypt_1.default.hash(password, 15);
-    const user = new user_1.default({
-        email,
-        password: hash
-    });
-    yield user.save();
+    try {
+        const { email, password } = req.body;
+        const userExits = yield user_1.default.findOne({ email });
+        if (userExits)
+            res.send("User with the same email already exists.");
+        else {
+            const hash = yield bcrypt_1.default.hash(password, 15);
+            const user = new user_1.default({
+                email,
+                password: hash
+            });
+            yield user.save();
+            res.send("ok");
+        }
+    }
+    catch (err) {
+        next(err);
+    }
 }));
 router.post('/login', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { email, password } = req.body;
+        const user = yield user_1.default.findOne({ email });
+        const validPassword = yield bcrypt_1.default.compare(password, user.password);
+        if (validPassword) {
+            res.send("ok");
+        }
+        else {
+            res.send("email or password incorrect");
+        }
+    }
+    catch (err) {
+        next(err);
+    }
 }));
 exports.default = router;
