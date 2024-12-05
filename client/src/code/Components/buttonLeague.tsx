c0b6d941ba040653c467939993b0a4a6c8bd30c5
joaoventura04 <joses4.js1@gmail.com>
2024-12-05 14:56:45 +0000

    Create buttonLeague.tsx

---
+++ client/src/code/Components/buttonLeague.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+interface AppProps {
+  setState: (state: { view: string }) => void;
+  imageSrc: string; 
+  label: string; 
+  isSubmitting?: boolean; 
+}
+
+const LoadingButton: React.FC<AppProps> = ({ setState, imageSrc, label }) => {
+  const redirectToLeague = () => {
+    setState({ view: "register" });
+  };
+
+  return (
+    <div>
+      <Button
+        size="sm"
+        variant="link"
+        type="submit"
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          fontSize: "0.9em",
+          display: "flex",
+          alignItems: "center",
+          padding: "5px 10px",
+          borderRadius: "5px",
+          border: "none"
+        }}
+        className="w-30 pb-2"
+        disabled={false} // Substitua `false` por `isSubmitting` se necessário
+        onClick={redirectToLeague}
+      >
+        <img
+          src={imageSrc} // A imagem agora vem da propriedade
+          alt="icon"
+          style={{ marginRight: "5px", width: "16px", height: "16px" }}
+        />
+        {label}
+      </Button>
+    </div>
+  );
+};
+
+export default LoadingButton;
